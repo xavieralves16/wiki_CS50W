5d67e9a61b4b945a8583d57f6fdd5f39dd77c75d
Xavier <xavieralves162@gmail.com>
2025-09-30 16:36:28 +0100

    Added search view in url paths

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("search", views.search, name="search"),
     path("wiki/<str:title>", views.entry_page, name="entry")
 ]
